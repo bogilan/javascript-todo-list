02b52febeb1b4d686ff3bbe58edc50864d00492b
bogilan <bogdan.lakicevic@gmail.com>
2023-03-13 20:30:17 +0100

    List items are now editable, clear list btn clears all list items, and list is saved to local storage

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const todoInput = document.querySelector('.todo-input');
 const addTodoBtn = document.querySelector('.add-todo-btn');
 const todoList = document.querySelector('.todo-list');
+const clearListBtn = document.querySelector('.clear-list-btn');
 
+window.addEventListener('load', () => {
+    todoList.innerHTML = localStorage.getItem('todos');
+})
 
 addTodoBtn.addEventListener('click', (e)=> {
     e.preventDefault();
@@ -9,9 +13,11 @@ addTodoBtn.addEventListener('click', (e)=> {
     if(todoItem) {
         const newTodo = document.createElement('li');
         newTodo.setAttribute("class", "todo-item");
-        newTodo.innerHTML = `<span class="todo-text">${todoItem}</span><span class="complete-todo">&#x2713;</span> <span class="delete-todo">&#128465;</span>`
+        newTodo.innerHTML = `<span class="todo-text" contentEditable="true">${todoItem}</span><span class="complete-todo">&#x2713;</span> <span class="delete-todo">&#128465;</span>`
         todoList.appendChild(newTodo);
         todoInput.value = '';
+
+        localStorage.setItem('todos', todoList.innerHTML);
     }
 });
 
@@ -20,9 +26,17 @@ todoList.addEventListener('click', (e)=> {
     if(e.target.classList.contains('delete-todo')) {
         todoItem.classList.add('deleted');
         setTimeout(()=> {
-            todoItem.remove()}, 800);
+            todoItem.remove();
+            localStorage.setItem('todos', todoList.innerHTML);
+        }, 800);
     }
     if(e.target.classList.contains('complete-todo')) {
         todoItem.classList.toggle('completed');
+        localStorage.setItem('todos', todoList.innerHTML);
     }
+})
+
+clearListBtn.addEventListener('click', ()=> {
+    todoList.innerHTML = '';
+    localStorage.setItem('todos', todoList.innerHTML);
 })
